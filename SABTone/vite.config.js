17419659082d8cd10f228d79a0de2fad1a9b46e7
Yasithb <150070850+Yasithb@users.noreply.github.com>
2025-05-16 09:48:38 +0530

    Create Navigation Bar

--- SABTone/vite.config.js
+++ SABTone/vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  
+  
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "brandPrimary": "#4CAF4F",
+        "neutralDGrey": "#4D4D4D",
+        "neutralGrey": "#717171",
+        "neutralSilver": "#F5F7FA",
+        "gray900": "#18191F",
+        "neutralBlack": "#263238",
+      }
+    },
+  },
+  
+  
+  
+  plugins: [react(),tailwindcss(),],
 })
